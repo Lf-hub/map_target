5d9f3c3b17f0a965a8e1af2a670457b91c2ea7ec
Luis Fernando Giglio <luisfgiglio@gmail.com>
2024-02-07 21:44:50 -0300

    fix template and map

--- core/views.py
+++ core/views.py
@@ -7,3 +7,22 @@ from core.models import Target
 class TargetIndex(ListView):
     model = Target
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['map_data'] = self.get_map_data()
+        return context
+
+    def get_map_data(self):
+        # Obtém dados para o mapa, neste exemplo, as coordenadas dos alvos
+        targets = Target.objects.all()
+        map_data = []
+
+        for target in targets:
+            map_data.append({
+                'nome': target.name,
+                'latitude': target.latitude,
+                'longitude': target.longitude,
+            })
+
+        return map_data
